9529a1d8ff88f5596608cad46f882d99a027ac7e
Eric <erictomich@gmail.com>
2023-01-03 22:10:45 -0300

    update projects

--- professional_dashboard/db_myprojects.js
+++ professional_dashboard/db_myprojects.js
@@ -2,7 +2,7 @@
 var myProjects = [];
 
 myProjects[0] = {
-    "title": "MyVocab",
+    "title": "VocabHub",
     "status": `Aguardando storytelling e definições para apresentar internamente. 
                 <br>Arquivo do storytelling:
                 <br><a href="https://www.figma.com/file/J9ueiwZAzswbD2PzxQpgKY/MyVocab-StoryTelling" targe="_blank">Arquivo storytelling</a>
@@ -41,7 +41,15 @@ myProjects[2] = {
 myProjects[3] = {
     "title": "Keeped IN",
     "status": `Protótipo em HTML`,
-    "descricao": `Sistema para salvar favoritos do Linkedin e fazer listas específicas.`
+    "descricao": `Sistema para salvar favoritos do Linkedin e fazer listas específicas.`,
+    "brainstorming": `Pensando em criar esse sistema com Next.js. <br>
+                    
+                    Usando SQLite para o protótipo, Sequelize como ORM. 
+                    
+                    <br>
+                    https://plainenglish.io/blog/next-js-with-sequelize-web-application-a-full-stack-web-development-a0051074e998
+                    <br>
+                    ` 
 }
 
 myProjects[4] = {
