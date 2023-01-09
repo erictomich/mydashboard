22c5592c14d2981147b00d6730197d3e1eca8337
Eric <erictomich@gmail.com>
2023-01-09 09:04:43 -0300

    update

--- professional_dashboard/db_myprojects.js
+++ professional_dashboard/db_myprojects.js
@@ -2,7 +2,7 @@
 var myProjects = [];
 
 myProjects[0] = {
-    "title": "VocabHub",
+    "title": "MyVocab",
     "status": `Aguardando storytelling e definições para apresentar internamente. 
                 <br>Arquivo do storytelling:
                 <br><a href="https://www.figma.com/file/J9ueiwZAzswbD2PzxQpgKY/MyVocab-StoryTelling" targe="_blank">Arquivo storytelling</a>
@@ -42,6 +42,7 @@ myProjects[1] = {
                   <br>/Estudo/learnprojects/nodejs/proj001_autenticacao_basica_passport
 
                   <br>é preciso criar uma pasta própria pra ele
+                  <br> já existe um repositório: https://github.com/erictomich/on-dashboard
 
                   <br><br> O projeto está usando: 
                   <br> NodeJS, Express, Sequelize (models, migration, seeders), SQLite, Engine ejs, autenticação
@@ -57,11 +58,23 @@ myProjects[2] = {
 }
 
 myProjects[2] = {
-    "title": "Kumon texts",
-    "status": `Transcrever todos os textos do kumon. Para que se possa mensurar a quantidade de palavras em cada história. 
-    Quantos termos novos existem.`,
-    "descricao": `Transcrever todos os textos do kumon. Para que se possa mensurar a quantidade de palavras em cada história. 
-    Quantos termos novos existem.`
+    "title": "Vocab Count",
+    "status": `em construção do protótipo`,
+    "descricao": `
+
+    O sistema irá permitir gravar sequências de textos em um idioma e fazer análise das palavras utilizadas.
+    Irá fazer uma análise de quais palavras novas estão surgindo a cada novo texto. 
+    Irá permitir pegar um texto externo e verificar quais palavras contém neste texto que não tem em um conjunto de textos específico. 
+
+    <br><br>
+    O projeto vocab count foi criado na pasta: 
+    <br>C:\\Users\\ericjanuzzi\\Estudo\\sideprojects\\vocab-count
+    <br>na rota "generate-texts", no arquivo "app.js", estão as anotações para os próximos passos.
+
+    <br><br>O projeto do kumon está em: <br>
+    C:\\Users\\ericjanuzzi\\Estudo\\sideprojects\\english-kumon-vocabulary
+
+    `
 }
 
 myProjects[3] = {
@@ -84,4 +97,9 @@ myProjects[4] = {
     "descricao": `Desenvolvimento cíclico da vida.`
 }
 
+myProjects[5] = {
+    "title": "CodeKataBR - Dev Poliglota",
+    "status": `Ideação`,
+    "descricao": `Irá conter inúmeros exercícios para fazer, para treinar linguagens de programação.`
+}
 
